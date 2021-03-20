519ea96cbd1f3b236767ccf81b740d2da79b71d8
Eduardo Aguirre <pokosh.xd@gmail.com>
2021-03-19 21:28:59 -0600

    creando carpeta para pagina de usuario

---
+++ user/infouser.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require('includes/head.php'); ?>
+
+<body>
+
+
+
+</div>
+    <?php require('includes/sidebar.php'); ?>
+    </div>
+    
+</body>
+
+</html>
