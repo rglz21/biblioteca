3aecab0e39faac22d7417a0d2fe949b179cd9eed
kristianedu <kristianedu31@gmail.com>
2021-03-22 00:54:15 -0600

    a

---
+++ admin/listarusuarios.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require('includes/head.php'); ?>
+
+<body>
+    <div class="wrapper ">
+        <?php require('includes/sidebar.php'); ?>
+        <div class="main-panel" style="height: 100vh;">
+            <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+                <div class="container-fluid">
+                    <div class="navbar-wrapper">
+                        <div class="navbar-toggle">
+                            <button type="button" class="navbar-toggler">
+                                <span class="navbar-toggler-bar bar1"></span>
+                                <span class="navbar-toggler-bar bar2"></span>
+                                <span class="navbar-toggler-bar bar3"></span>
+                            </button>
+                        </div>
+                        <a class="navbar-brand" href="javascript:;">Nsssasasuevo Libro</a>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                        aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                        <form>
+                            <div class="input-group no-border">
+                                <input type="text" value="" class="form-control" placeholder="Search...">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <i class="nc-icon nc-zoom-split"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <ul class="navbar-nav">
+                            <li class="nav-item btn-rotate dropdown">
+                                <a class="nav-link dropdown-toggle" href="http://example.com"
+                                    id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
+                                    <i class="nc-icon nc-bell-55"></i>
+                                    <p>
+                                        <span class="d-lg-none d-md-block">Some Actions</span>
+                                    </p>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="content">
+                <div class="card" id="esfera">
+      <?php if (isset($_SESSION['message'])) { ?>
+                <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
+                    <?= $_SESSION['message']?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+            </div>
+      <?php session_unset(); } ?>
+                    <div class="card-body">
+                        <h2 class="card-title">Ingrese informacion del libro</h2>
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <div class="mb-3">
+                                <form action="taksAdmin.php"  method="post">
+                                    <img src="https://image.freepik.com/vector-gratis/libro-blanco-sobre-fondo-blanco_1308-23052.jpg"
+                                        class="img-thumbnail" alt="...">
+                                    <br>
+                                    <br>
+                                    <label for="formFile" class="form-label">ingrese foto del libro</label>
+                                    <input class="form-control" type="file" name="imagen">
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                
+                                    <div class="row">
+                                        <div class="col">
+                                             <form action="" method="post">
+                                            <div class="form-floating mb-3">
+                                                <label for="exampleFormControlInput1" class="floatingInput">Cod
+                                                    Libro</label>
+                                                <input type="text" class="form-control" placeholder="Codigo"
+                                                    name="cod" />
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <label for="exampleFormControlInput1" class="floatingInput">Nombre del
+                                                    libro</label>
+                                                <input type="text" class="form-control" placeholder="Nombre"
+                                                    name="nombre" />
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <label for="exampleFormControlInput1"
+                                                    class="floatingInput">Autor</label>
+                                                <input type="text" class="form-control" placeholder="Autor"
+                                                    name="autor" />
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <label for="exampleFormControlInput1" class="floatingInput">Año</label>
+                                                <input type="text" class="form-control" placeholder="Año" name="anio" />
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <label for="exampleFormControlInput1"
+                                                    class="floatingInput">Editorial</label>
+                                                <input type="text" class="form-control" placeholder="Editorial"
+                                                    name="editorial" />
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <label for="exampleFormControlInput1" class="floatingInput">Nº
+                                                    Disponible</label>
+                                                <input type="text" class="form-control"
+                                                    placeholder="Cantidad Disponible" name="cantidad" />
+                                            </div>
+                                            <input type="submit" name="nLibro" class="btn btn-success btn-block"
+                                                value="Agregar Libro">
+                                            </form>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <?php require('includes/sidebar.php'); ?>
+    </div>
+    
+</body>
+
+</html>
