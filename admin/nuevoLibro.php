d39e5af9cb46d3c4ebd92a383ea77dfc792497a2
kristianedu <kristianedu31@gmail.com>
2021-05-03 16:41:29 -0600

    Update nuevoLibro.php

--- admin/nuevoLibro.php
+++ admin/nuevoLibro.php
@@ -89,7 +89,7 @@
                                                 <label for="exampleFormControlInput1" class="floatingInput">Cod
                                                     Libro</label>
                                                 <input type="text" class="form-control" placeholder="Codigo"
-                                                    name="cod" />
+                                                    name="cod" value="<?php echo rand();  ?>"/>
                                             </div>
                                             <div class="form-floating mb-3">
                                                 <label for="exampleFormControlInput1" class="floatingInput">Nombre del
